4ffcc57a20aeac0f27e30924c8b0a2ee18ac3bfa
geethakalaiselvan2303 <geethakalaiselvan2303@gmail.com>
2023-08-01 21:38:35 +0530

    print 0-99 without using number

---
+++ src/main/java/MandatoryHomeWork/Foundation/Print0_99.java
@@ -0,0 +1,9 @@
+package MandatoryHomeWork.Foundation;
+
+public class Print0_99 {
+	public static void main(String[] args) {
+		for (char i = '\0'; i<='c' ; i++) {
+			System.out.println((int)i);
+		}
+	}
+}
