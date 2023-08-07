8cb5fa722c0ef8c31470e661a9402027b09d9d46
geethakalaiselvan2303 <geethakalaiselvan2303@gmail.com>
2023-08-07 19:32:53 +0530

    Second largest number

---
+++ src/main/java/MandatoryHomeWork/MentorClassroomSession/SecondLargestNumber_way2.java
@@ -0,0 +1,27 @@
+package MandatoryHomeWork.MentorClassroomSession;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SecondLargestNumber_way2 {
+
+	public static void main(String[] args) {
+		int result=0;
+		int[] arr = new int[] {4,3,1,202,100,85,2,122,200};
+		int temp = arr[0];
+		int temp1 = arr[0];
+		int rev = 0;
+		for (int i = 1; i < arr.length; i++) {
+			if (temp<arr[i]) {
+				temp1=temp;
+				temp=arr[i];
+			}
+			else if(arr[i]>temp1) {
+				temp1 = arr[i];
+				}
+			}
+		System.out.println(temp1);
+	}
+
+}
