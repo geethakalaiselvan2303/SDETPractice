51db4d5c55f15d074e59b0b1c9a508842cb5bd5f
geethakalaiselvan2303 <geethakalaiselvan2303@gmail.com>
2023-10-06 18:47:24 +0530

    oct 6

--- src/main/java/mandatoryHomeWork/MentorClassroomSession/Mentor_DSA/HappyNumbers.java
+++ src/main/java/mandatoryHomeWork/MentorClassroomSession/Mentor_DSA/HappyNumbers.java
@@ -14,15 +14,15 @@ public class HappyNumbers {
 		if yes get the remainder of the given number 
 		then take the square of the remainder(n%10) and add with sum
 		then divide the given number and store it in the given variable name itself(n/10)
-		the loop iterartes take remainder(n%10) and quotient(n/10) still it the given number reaches 
-	3. Each iteration check wheather n reaches 0
+		the loop iterates take remainder(n%10) and quotient(n/10) still it the given number reaches 
+	3. Each iteration check whether n reaches 0
 		if yes assign the n to sum
 		check whether the sum==1 If yes return true
 		then also add the number in the set to check whether the number happy number or not 
 		the reset the sum as 0
 		
 		Eg:--> 2(is not a happy number because when calculate the happy number as follows
-				2,4,16,37,58,89,145,42,20,4-->it start with 4 again it reachiing the 4
+				2,4,16,37,58,89,145,42,20,4-->it start with 4 again it reaching the 4
 				So the loop continues it will not end
 				because of that to break the condition we are adding the values in the set when we add the already exist number then the condition return false.
 
